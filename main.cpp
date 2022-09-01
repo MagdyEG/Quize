ca714bcdf8c841372d181a16a8004d6b5082eddd
Mohamed magdy <56060941+MagdyEG@users.noreply.github.com>
2022-09-01 12:46:35 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int main() {
+
+	for (int i = 1; i <= 2; i++) {
+
+		for (int j = 1; j <= 3; j++)
+        cout<< (i * j);
+		cout<< endl;}
+	}
